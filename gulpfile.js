6db9dacd71589e21d58960cc4139cb94f6b1822c
Cole Bemis <info@colebemis.com>
2015-05-17 11:38:51 -0700

    Increment version number

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var nib = require('nib');
 var stylus = require('gulp-stylus');
 var zip = require('gulp-zip');
 
-var version = '1.0.4';
+var version = '1.0.5';
 
 var paths = {
   html: './*.html',
