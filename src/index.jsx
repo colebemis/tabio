fcae7875b3c7e91a1419c8b6d19204c56ace0911
Cole Bemis <colebemis@gmail.com>
2017-07-26 11:32:29 -0700

    Convert App into a stateful component

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-const App = () => <h1>Hello Tabio</h1>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { foo: 'bar' };
+  }
+
+  render() {
+    return (
+      <h1>
+        {this.state.foo}
+      </h1>
+    );
+  }
+}
 
 render(<App />, document.getElementById('root'));
