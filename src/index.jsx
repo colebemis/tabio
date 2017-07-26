3422c489b86d0db1de5fab198a1dd19d172fcd3b
Cole Bemis <colebemis@gmail.com>
2017-07-26 12:29:33 -0700

    Get tab data

--- src/index.jsx
+++ src/index.jsx
@@ -5,14 +5,22 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { foo: 'bar' };
+    this.state = { tabGroups: [] };
+  }
+
+  componentDidMount() {
+    chrome.windows.getAll({ populate: true }, tabGroups => {
+      this.setState({ tabGroups });
+    });
   }
 
   render() {
     return (
-      <h1>
-        {this.state.foo}
-      </h1>
+      <pre>
+        <code>
+          {JSON.stringify(this.state.tabGroups, null, 2)}
+        </code>
+      </pre>
     );
   }
 }
